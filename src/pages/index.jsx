49a498d3376e72a9ad484a3f2845c4e2570ae382
dcgleason <danny.c.gleason@gmail.com>
2023-08-10 15:06:31 -0400

    typo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -279,7 +279,7 @@ export default function Home({ articles }) {
             Software developer,  learner, and creator
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          I’m Danny, a developer based in Connecticut. I work as a developer in the ServiceNow now and enjoy developing mobile and web apps using many different tools and frameworks in my free time.  
+          I’m Danny, a developer based in Connecticut. I work as a developer in the ServiceNow space and enjoy developing mobile and web apps using many different tools and frameworks in my free time.  
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
